8b16ca324428c485ec89156bb0a8150ee461a069
Valentyn Slivko <demigorrgon@gmail.com>
2020-11-24 17:09:59 +0200

    Added test case using requests

---
+++ tests/test_with_requests.py
@@ -0,0 +1,17 @@
+import unittest
+import requests
+from main import dummy
+
+PATH = 'http://localhost:5000/'
+
+
+class ApiTestCase(unittest.TestCase):
+	def test_get_method(self):
+		response = requests.get(PATH + 'api/departments/2')
+		assert response.headers['Content-Type'] == 'application/json'
+		response_body = response.json()
+		assert response_body['id'] == 2
+
+	def test_put_method(self):
+		response = requests.put(PATH + 'api/departments/9', data={'id': 9, 'department': 'Another one'}).json()
+		# assert
